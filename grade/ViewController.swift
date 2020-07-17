a90c6a487436f1fb57902ddc2e5bfa8672bdefa0
Darian Davis <dariandavis788@gmail.com>
2020-07-17 13:12:43 -0500

    this is grade
    this new

--- grade/ViewController.swift
+++ grade/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var finalGrade: UILabel!
+    
+    @IBOutlet weak var currentGradeTextFeild: UITextField!
+    
+    @IBOutlet weak var DesireGradeTexTField: UITextField!
+    
+    @IBOutlet weak var enterPercentageTextField: UITextField!
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        let currentGrade = Double(currentGradeTextFeild.text!)
+        
+        let desireGrade =
+            Double(DesireGradeTexTField.text!)
+        
+        let enterPercentage =
+        Double(enterPercentageTextField.text!)
+        
+        let onehundred = 100.0
+        
+        let percentOfGrade = Double(onehundred - finalGradePercentage!)/oneHundred
+        
+        let gradeNeededOnFinal = (desireGrade! - (percentOfGrade * currentGrade !))/finalWorthPercentage!)/100.0
+        
+        gradeNeededOnFinal.text = "You need a "\(gradeNeededOnFinal * 100)%
+        
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
